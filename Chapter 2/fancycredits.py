413218aa3c247f3c9fb5150f34de22b61aeaca2a
skipy <skipy.devt@outlook.com>
2018-06-03 12:26:40 +0100

    initial commit

---
+++ Chapter 2/fancycredits.py
@@ -0,0 +1,17 @@
+# Fancy Credits
+# example escape sequences
+
+print ("\t\t\tFancy Credits")
+
+print ("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print ("\t\t\t\tby")
+print ("\t\t\tMichael Dawson")
+print ("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+
+print ("\nSpecial Thanks go out to:")
+print ("My hair stylist, Henry \'The Great,\' who never says \"can\'t.\"")
+
+#sound the bell
+print ("\a")
+
+input ("\n\nPress enter key to exit.")
---
+++ README.txt
@@ -0,0 +1,9 @@
+# learning
+
+This is a repository for storage of Python training materials I am working 
+through, while also learning a bit about how git works and what github can
+offer. 
+
+simple code.
+
+simple reason for living.
